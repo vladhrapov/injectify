6d8545b2122f7fd8226fdf7ea6e005389325fdf5
Vladyslav Khrapov <vlad.hrapov@gmail.com>
2020-09-12 23:05:29 +0300

    feat(injectify.abstractions): remove services property from IStartup api, update ConfigureServices to return void

--- packages/Injectify.Abstractions/Injectify.Abstractions/IStartup.cs
+++ packages/Injectify.Abstractions/Injectify.Abstractions/IStartup.cs
@@ -4,19 +4,12 @@
     /// Register dependencies for the application startup.
     /// </summary>
     /// <typeparam name="TServiceCollection">Service collection type.</typeparam>
-    /// <typeparam name="TServiceProvider">Service provider with registered services.</typeparam>
-    public interface IStartup<TServiceCollection, TServiceProvider>
+    public interface IStartup<TServiceCollection>
     {
-        /// <summary>
-        /// Service provider with a list of registered services.
-        /// </summary>
-        TServiceProvider Services { get; }
-
         /// <summary>
         /// Configure services.
         /// </summary>
         /// <param name="services">Collection of the registered services.</param>
-        /// <returns>Service provider with registered services.</returns>
-        TServiceProvider ConfigureServices(TServiceCollection services);
+        void ConfigureServices(TServiceCollection services);
     }
 }
