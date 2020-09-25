0b59ff0fe323ad213cc32ab0dafcee4193ca3b54
Vladyslav Khrapov <vlad.hrapov@gmail.com>
2020-09-25 22:24:32 +0300

    feat(samples): add usage sample of 0.3.0 version

---
+++ samples/SampleAppV0/SomePage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Injectify.Microsoft.DependencyInjection;
+using SampleAppV0.Services;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace SampleAppV0
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    [Injectable]
+    public sealed partial class SomePage : Page
+    {
+        public SomePage()
+        {
+            this.InitializeComponent();
+        }
+
+        [Inject]
+        public ISampleService SampleService { get; set; }
+    }
+}
