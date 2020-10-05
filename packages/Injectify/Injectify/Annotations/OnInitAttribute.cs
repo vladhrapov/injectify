708488002433a41ab04aed776ce0172db99f3d77
Vladyslav Khrapov <vlad.hrapov@gmail.com>
2020-10-05 23:05:50 +0300

    feat(injectify): add onInit common implementation, introspection of marked methods using OnInit annotation

---
+++ packages/Injectify/Injectify/Annotations/OnInitAttribute.cs
@@ -0,0 +1,36 @@
+﻿using Injectify.Abstractions;
+using Injectify.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Injectify.Annotations
+{
+    [AttributeUsage(AttributeTargets.Method, Inherited = false, AllowMultiple = false)]
+    public sealed class OnInitAttribute : Attribute, IOnInit
+    {
+        public void Bootstrap<TPage, TServiceProvider>(TPage page,
+            MethodInfo onInitMethodInfo,
+            TServiceProvider serviceProvider,
+            Func<TServiceProvider, ParameterInfo, object> serviceSelector)
+                where TPage : class
+        {
+            var parameters = onInitMethodInfo.GetParameters();
+
+            if (!parameters.Any())
+                return;
+
+            var paramsInstances = new List<object>(parameters.Length);
+
+            foreach (var parameter in parameters)
+            {
+                var argumentInstance = serviceSelector(serviceProvider, parameter);
+                paramsInstances.Add(argumentInstance);
+            }
+
+            onInitMethodInfo.Invoke(page, paramsInstances.ToArray());
+        }
+    }
+}
---
+++ packages/Injectify/Injectify/Helpers/BootstrapHelper.cs
@@ -0,0 +1,29 @@
+﻿using Injectify.Annotations;
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Injectify.Microsoft.DependencyInjection")]
+
+namespace Injectify.Helpers
+{
+    internal sealed class BootstrapHelper
+    {
+        public static void BootstrapInitParams<TPage, TServiceProvider>(TPage page,
+            TServiceProvider serviceProvider,
+            Func<TServiceProvider, ParameterInfo,  object> serviceSelector)
+                where TPage : class
+        {
+
+            var onInitMethodInfo = IntrospectionHelper.GetOnInitMethod(page);
+            var onInitMethod = onInitMethodInfo?.GetCustomAttribute<OnInitAttribute>();
+
+            if (onInitMethod is null)
+            {
+                return;
+            }
+
+            onInitMethod.Bootstrap(page, onInitMethodInfo, serviceProvider, serviceSelector);
+        }
+    }
+}
