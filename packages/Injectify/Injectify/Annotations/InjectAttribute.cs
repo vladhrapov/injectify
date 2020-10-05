4dd4416ccaf678bccc46264e5c6f4f0a65ff96e7
Vladyslav Khrapov <vlad.hrapov@gmail.com>
2020-10-05 23:50:04 +0300

    feat(injectify): move common implementation of the InjectAttribute from Injectify.Microsoft.DependencyInjection to the Injectify package

---
+++ packages/Injectify/Injectify/Annotations/InjectAttribute.cs
@@ -0,0 +1,34 @@
+﻿using Injectify.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Injectify.Annotations
+{
+    /// <summary>
+    /// Marks property of the class for injection.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property, Inherited = false, AllowMultiple = false)]
+    public sealed class InjectAttribute : Attribute, IInject
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="TPage"></typeparam>
+        /// <typeparam name="TServiceProvider"></typeparam>
+        /// <param name="page"></param>
+        /// <param name="propInfo"></param>
+        /// <param name="serviceProvider"></param>
+        /// <param name="serviceSelector"></param>
+        public void Bootstrap<TPage, TServiceProvider>(TPage page,
+            PropertyInfo propInfo,
+            TServiceProvider serviceProvider,
+            Func<TServiceProvider, PropertyInfo, object> serviceSelector)
+        {
+            var propertyInstance = serviceSelector(serviceProvider, propInfo);
+            propInfo.SetValue(page, propertyInstance);
+        }
+    }
+}
--- packages/Injectify/Injectify/Helpers/BootstrapHelper.cs
+++ packages/Injectify/Injectify/Helpers/BootstrapHelper.cs
@@ -1,5 +1,6 @@
 ﻿using Injectify.Annotations;
 using System;
+using System.Linq;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 
@@ -9,12 +10,30 @@ namespace Injectify.Helpers
 {
     internal sealed class BootstrapHelper
     {
+        public static void BootstrapProps<TPage, TServiceProvider>(TPage page,
+            TServiceProvider serviceProvider,
+            Func<TServiceProvider, PropertyInfo, object> serviceSelector)
+        {
+            var injectPropsInfo = page.GetType()
+                .GetProperties()
+                .Where(pi => pi.GetCustomAttribute<InjectAttribute>() != null)
+                .ToArray();
+
+            if (!injectPropsInfo.Any())
+                return;
+
+            foreach (var propInfo in injectPropsInfo)
+            {
+                var inject = propInfo.GetCustomAttribute<InjectAttribute>();
+                inject.Bootstrap(page, propInfo, serviceProvider, serviceSelector);
+            }
+        }
+
         public static void BootstrapInitParams<TPage, TServiceProvider>(TPage page,
             TServiceProvider serviceProvider,
             Func<TServiceProvider, ParameterInfo,  object> serviceSelector)
                 where TPage : class
         {
-
             var onInitMethodInfo = IntrospectionHelper.GetOnInitMethod(page);
             var onInitMethod = onInitMethodInfo?.GetCustomAttribute<OnInitAttribute>();
 
