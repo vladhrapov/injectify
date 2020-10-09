2cf6ce69ff58413db0843042f0a166e00226870a
Vladyslav Khrapov <vlad.hrapov@gmail.com>
2020-10-09 23:03:52 +0300

    refactor(injectify.microsoft.dependencyinjection): fix typo of the bootstrapper name

--- packages/Injectify.Microsoft.DependencyInjection/Injectify.Microsoft.DependencyInjection/Extensions/Bootstrapper.cs
+++ packages/Injectify.Microsoft.DependencyInjection/Injectify.Microsoft.DependencyInjection/Extensions/Bootstrapper.cs
@@ -12,18 +12,16 @@ namespace Injectify.Microsoft.DependencyInjection.Extensions
     /// <summary>
     /// 
     /// </summary>
-    public static class Bootstraper
+    public static class Bootstrapper
     {
         /// <summary>
         /// 
         /// </summary>
         /// <typeparam name="TPage"></typeparam>
         /// <param name="page"></param>
-        [Obsolete("Call of this method starting from v0.3.0 is redundant and outdated. It will be removed starting from the major version 1.0.0.")]
-        public static void Bootstrap<TPage>(this TPage page)
+        public static TPage Bootstrap<TPage>(this TPage page)
             where TPage : Page
         {
-            //BootstrapApp();
             var classInjectable = page.GetType().GetCustomAttribute<InjectableAttribute>();
 
             var serviceProvider = IntrospectionHelper.GetServiceProviderFromApplication<ServiceProvider>(Application.Current);
@@ -33,6 +31,8 @@ namespace Injectify.Microsoft.DependencyInjection.Extensions
                 ServiceProviderExtensions.GetByParameterInfo);
 
             classInjectable.Bootstrap(context);
+
+            return page;
         }
     }
 }
