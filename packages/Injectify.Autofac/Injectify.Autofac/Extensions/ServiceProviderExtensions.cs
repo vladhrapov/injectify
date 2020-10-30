d6f8d73e63efdef9cfbbee983f0e58709eb4037e
Vladyslav Khrapov <vlad.hrapov@gmail.com>
2020-10-31 01:36:06 +0200

    feat(injectify.autofac): move common implementation from Injectify.Autofac to Injectify common package

--- packages/Injectify.Autofac/Injectify.Autofac/Extensions/ServiceProviderExtensions.cs
+++ packages/Injectify.Autofac/Injectify.Autofac/Extensions/ServiceProviderExtensions.cs
@@ -15,15 +15,8 @@ namespace Injectify.Autofac.Extensions
         /// <param name="provider"></param>
         /// <param name="propInfo"></param>
         /// <returns></returns>
-        public static object GetByPropertyInfo(this IContainer provider, PropertyInfo propInfo)
-        {
-            if (propInfo.PropertyType?.GenericTypeArguments?.Any() ?? false)
-            {
-                //return provider.GetServices(propInfo.PropertyType?.GenericTypeArguments?.FirstOrDefault());
-            }
-
-            return provider.Resolve(propInfo.PropertyType);
-        }
+        public static object GetByPropertyInfo(this IContainer provider, PropertyInfo propInfo) =>
+            provider.Resolve(propInfo.PropertyType);
 
         /// <summary>
         /// 
@@ -31,14 +24,7 @@ namespace Injectify.Autofac.Extensions
         /// <param name="provider"></param>
         /// <param name="parameterInfo"></param>
         /// <returns></returns>
-        public static object GetByParameterInfo(this IContainer provider, ParameterInfo parameterInfo)
-        {
-            if (parameterInfo.ParameterType?.GenericTypeArguments?.Any() ?? false)
-            {
-                //return provider.GetServices(parameterInfo.ParameterType?.GenericTypeArguments?.FirstOrDefault());
-            }
-
-            return provider.Resolve(parameterInfo.ParameterType);
-        }
+        public static object GetByParameterInfo(this IContainer provider, ParameterInfo parameterInfo) =>
+            provider.Resolve(parameterInfo.ParameterType);
     }
 }
--- packages/Injectify.Autofac/Injectify.Autofac/UwpApplicationBootstrapAttribute.cs
+++ packages/Injectify.Autofac/Injectify.Autofac/UwpApplicationBootstrapAttribute.cs
@@ -1,39 +1,13 @@
-﻿using Autofac;
-using Injectify.Abstractions;
-using System;
-using System.Linq;
-using System.Reflection;
-using Windows.UI.Xaml;
+﻿using System;
 
 namespace Injectify.Autofac
 {
     /// <summary>
-    /// 
+    /// Annotation for Application bootstrap class.
     /// </summary>
+    [Obsolete("This attribute is going to be deleted in v1.0.0")]
     [AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
     public sealed class UwpApplicationBootstrapAttribute : Attribute
     {
-        /// <summary>
-        /// Bootstrap application marked by UwpApplicationBootstrap.
-        /// </summary>
-        /// <param name="application"></param>
-        /// <param name="startup"></param>
-        public void Bootstrap(Application application, IStartup<ContainerBuilder> startup)
-        {
-            var uwpAppClass = Assembly.GetEntryAssembly()
-                .GetTypes()
-                .Where(t => t.IsClass && (t.BaseType == typeof(Application))) // ToDo: Maybe better to check whether implements IUwpApplication
-                .FirstOrDefault();
-
-            var servicesProp = uwpAppClass.GetProperties()
-                .Where(p => p.PropertyType == typeof(IContainer))
-                .FirstOrDefault();
-
-            var services = new ContainerBuilder();
-            startup.ConfigureServices(services);
-            var provider = services.Build();
-
-            servicesProp.SetValue(application, provider);
-        }
     }
 }
