ef6ba1f6f55ee6a787edc4ace4bd76ccdf2434d7
Vladyslav Khrapov <vlad.hrapov@gmail.com>
2022-11-30 01:31:15 +0100

    chore: rename packages to src (#10)

---
+++ src/Injectify.Autofac/Extensions/ServiceProviderExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Autofac;
+using System.Linq;
+using System.Reflection;
+
+namespace Injectify.Autofac.Extensions
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    internal static class ServiceProviderExtensions
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="provider"></param>
+        /// <param name="propInfo"></param>
+        /// <returns></returns>
+        public static object GetByPropertyInfo(this IContainer provider, PropertyInfo propInfo) =>
+            provider.Resolve(propInfo.PropertyType);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="provider"></param>
+        /// <param name="parameterInfo"></param>
+        /// <returns></returns>
+        public static object GetByParameterInfo(this IContainer provider, ParameterInfo parameterInfo) =>
+            provider.Resolve(parameterInfo.ParameterType);
+    }
+}
---
+++ src/Injectify.Microsoft.DependencyInjection/FrameWithServiceProvider.cs
@@ -0,0 +1,43 @@
+﻿using Injectify.Abstractions;
+using Injectify.Annotations;
+using Injectify.Microsoft.DependencyInjection.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Reflection;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace Injectify.Microsoft.DependencyInjection
+{
+    internal class FrameWithServiceProvider : Frame
+    {
+        private readonly ServiceProvider _serviceProvider;
+
+        public FrameWithServiceProvider(ServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider ?? throw new ArgumentNullException(nameof(serviceProvider));
+            //this.Navigated += OnFrameNavigated;
+        }
+
+        protected virtual void OnFrameNavigated(object sender, NavigationEventArgs e)
+        {
+            var classInjectable = e.SourcePageType.GetCustomAttribute<InjectableAttribute>();
+
+            // No need to throw when class was not marked as Injectable.
+            // Just do not need to bootstrap dependencies.
+            if (classInjectable != null)
+            {
+                if (!(e.Content is Page))
+                    throw new InvalidCastException($"'{e.Content.GetType().Name}' is not assignable to '{typeof(Page).Name}'");
+
+                //var serviceProvider = IntrospectionHelper.GetServiceProviderFromApplication<ServiceProvider>(Application.Current);
+                var context = new InjectionContext<Page, ServiceProvider>(e.Content as Page,
+                    _serviceProvider,
+                    ServiceProviderExtensions.GetByPropertyInfo,
+                    ServiceProviderExtensions.GetByParameterInfo);
+
+                classInjectable.Bootstrap(context);
+            }
+        }
+    }
+}
